707eb4bb11c0eab0e6195e693b290997220309b5
Amruta Jahagirdar <ajahagirdar8@gmail.com>
2020-08-24 07:25:39 +0530

    first

---
+++ UnionFindWeighted/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace QuickUnion
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			QuickUnionWtd qu = new QuickUnionWtd (6);
+
+			bool b = qu.isConnected (1, 2);
+
+			Console.WriteLine ("1, 2 " + b);
+
+			qu.Union (1, 2);
+			Console.WriteLine ("1, 2 " + b);
+
+		    b = qu.isConnected (2, 3);
+
+			Console.WriteLine ("2, 3 " + b);
+
+			qu.Union (2, 3);
+			Console.WriteLine ("2, 3 " + b);
+
+			Console.WriteLine ("Size[1] " + qu.getTreeSize (1));
+			Console.WriteLine ("Size[2] " + qu.getTreeSize (2));
+			Console.WriteLine ("Size[3] " + qu.getTreeSize (3));
+
+		}
+	}
+}
---
+++ UnionFindWeighted/QuickFindWtd.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace QuickUnion
+{
+	public class QuickUnionWtd
+	{
+		int[] ID;
+		int[] Size;
+
+		public QuickUnionWtd (int N)
+		{
+			ID = new int[N];
+			Size = new int[N];
+
+			for (int i = 0; i < N; i++) {
+
+				ID [i] = i;
+				Size[i]= 1;
+			}
+		}
+
+		public int getTreeSize(int n)
+		{
+			return Size [n];
+		}
+		public int getRoot(int p)
+		{
+			while (ID [p] != p)
+				p = ID [p];
+
+			return p;
+		}
+
+		public bool isConnected(int p, int q)
+		{
+			return getRoot (p) == getRoot (q);
+		}
+
+		public void Union(int p, int q)
+		{
+			int rootP = getRoot (p);
+			int rootQ = getRoot (q);
+
+			if (Size [rootP] == Size [rootQ])
+				ID [rootP] = rootQ;
+			else if (Size [rootP] > Size [rootQ]) {
+				ID [rootQ] = rootP;
+				Size [rootP] += Size [rootQ];
+			} else {
+				ID [rootP] = rootQ;
+				Size [rootQ] += Size [rootP];
+			}
+
+		}
+			
+
+	}
+}
+
